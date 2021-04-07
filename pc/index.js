fef0555142b69d43024dbe46bd5dffc91434238c
unknown <nathan01010000@gmail.com>
2021-04-07 12:30:05 -0700

    RPi weebserver

---
+++ pc/index.js
@@ -0,0 +1,34 @@
+const {exec} = require('child_process');
+const app = require('express')();
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.send('cum');
+});
+
+app.post('/onFor/:millis', (req, res) => {
+    //exec('sudo python gpio/on.py');
+
+    setTimeout(() => {
+        console.log(`cum after ${req.params.millis}`);
+    }, req.params.millis);
+    res.send(200);
+});
+
+app.post('/on', (req, res) => {
+    exec('sudo python gpio/on.py');
+    res.send(200);
+});
+
+app.post('/off', (req, res) => {
+    exec('sudo python gpio/off.py');
+    res.send(200);
+});
+
+app.post('brrr', (req, res) => {
+    //Process electroncution
+
+    res.send(200);
+});
+
+app.listen(3000, console.log('Server Listening in 3000'));
